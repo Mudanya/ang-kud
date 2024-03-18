1b286b718bfce6021efd9347f52241bfc573274b
Mudanya <nmudanya94@gmail.com>
2024-03-18 23:52:25 +0300

    Adding Custom Validators

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { log } from 'console';
 
 @Component({
@@ -10,31 +10,107 @@ import { log } from 'console';
 export class CreateEmployeeComponent implements OnInit {
   employeeForm:FormGroup
   constructor(private fb:FormBuilder) { }
+  validationMessages = {
+    fullName:{
+      required: 'Full Name is required!',
+      minlength: 'Full Name should be greater than 2',
+      maxlength: 'Full Name should be less than 10',
+    },
+    email:{
+      required: 'Email is required'
+    },
+    skillName:{
+      required: 'Skill is required'
+    },
+    experienceInYears:{
+      required: 'Experience is required'
+    },
+    proficiency:{
+      required: 'Proficiency is required'
+    },
+    phone:{
+      required: 'Phone is required'
+    },
+
+  }
+
+  formErrors = {
+    fullName:'',
+    email: '',
+    skillName: '',
+    experienceInYears: '',
+    proficiency: '',
+    phone:''
+
+  }
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-      email: [''],
+      email: ['',Validators.required],
+      phone: [''],
+      contactPreference: ['email'],
       skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['']
+        skillName: ['',Validators.required],
+        experienceInYears: ['',Validators.required],
+        proficiency: ['',Validators.required]
       })
     })
-  }
+
+    this.employeeForm.get('contactPreference').valueChanges.subscribe((data:string) => {
+      debugger
+      this.validatePhone(data)
+    })
+
+    this.employeeForm.valueChanges.subscribe(
+      data => this.logValidationErrors(this.employeeForm)
+      )
+  } 
   onSubmit() : void {
     console.log(this.employeeForm);
     
   }
-  onLoadData() : void {
-    this.employeeForm.patchValue({
-      fullName:'Nexo',
-      email: 'nexo@gmail.com',
-      // skills:{
-      //   skillName:'Developer',
-      //   experienceInYears:5,
-      //   proficiency:'beginner'
-      // }
+  validatePhone(selectedValue:string) {
+    debugger
+    const phone = this.employeeForm.get('phone')
+    const email = this.employeeForm.get('email')
+    if(selectedValue == 'phone') {
+      email.clearValidators()
+      phone.setValidators(Validators.required)
+      phone.updateValueAndValidity()
+    }
+    else {
+      phone.clearValidators()
+      email.setValidators(Validators.required)
+    }
+    phone.updateValueAndValidity()
+  }
+  logValidationErrors(group: FormGroup = this.employeeForm) : void {
+    Object.keys(group.controls).forEach((key:string) => 
+    {
+      const abstractControl  =  group.get(key)
+      if(abstractControl instanceof FormGroup)
+      {
+        this.logValidationErrors(abstractControl)
+      }
+      else {
+          this.formErrors[key] = ''
+          if(abstractControl && !abstractControl.valid && (abstractControl.dirty || abstractControl.touched))
+          {
+            const messages = this.validationMessages[key]
+            const errors = abstractControl.errors
+            if(errors)
+            {
+              for(const errorKey in errors)
+              {
+                this.formErrors[key] += messages[errorKey] + ' '
+              }
+            }
+          }
+      }
     })
+  }
+  onLoadData() : void {
+    
   }
 }
