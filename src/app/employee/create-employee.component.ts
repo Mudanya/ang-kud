1e81d95e14e1702cb697bfc1f29abe2c7e72f8a2
Mudanya <nmudanya94@gmail.com>
2024-03-19 22:12:28 +0300

    Custom validator

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,116 +1,134 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { log } from 'console';
+import { Component, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { log } from "console";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"],
 })
 export class CreateEmployeeComponent implements OnInit {
-  employeeForm:FormGroup
-  constructor(private fb:FormBuilder) { }
+  employeeForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
   validationMessages = {
-    fullName:{
-      required: 'Full Name is required!',
-      minlength: 'Full Name should be greater than 2',
-      maxlength: 'Full Name should be less than 10',
+    fullName: {
+      required: "Full Name is required!",
+      minlength: "Full Name should be greater than 2",
+      maxlength: "Full Name should be less than 10",
     },
-    email:{
-      required: 'Email is required'
+    email: {
+      required: "Email is required",
+      emailDomain: "Wrong Domain nexo.com is required",
     },
-    skillName:{
-      required: 'Skill is required'
+    skillName: {
+      required: "Skill is required",
     },
-    experienceInYears:{
-      required: 'Experience is required'
+    experienceInYears: {
+      required: "Experience is required",
     },
-    proficiency:{
-      required: 'Proficiency is required'
+    proficiency: {
+      required: "Proficiency is required",
     },
-    phone:{
-      required: 'Phone is required'
+    phone: {
+      required: "Phone is required",
     },
-
-  }
+  };
 
   formErrors = {
-    fullName:'',
-    email: '',
-    skillName: '',
-    experienceInYears: '',
-    proficiency: '',
-    phone:''
-
-  }
+    fullName: "",
+    email: "",
+    skillName: "",
+    experienceInYears: "",
+    proficiency: "",
+    phone: "",
+  };
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
-      fullName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-      email: ['',Validators.required],
-      phone: [''],
-      contactPreference: ['email'],
+      fullName: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(10),
+        ],
+      ],
+      email: ["", [Validators.required, emailDomain('nexo.com')]],
+      phone: [""],
+      contactPreference: ["email"],
       skills: this.fb.group({
-        skillName: ['',Validators.required],
-        experienceInYears: ['',Validators.required],
-        proficiency: ['',Validators.required]
-      })
-    })
+        skillName: ["", Validators.required],
+        experienceInYears: ["", Validators.required],
+        proficiency: ["", Validators.required],
+      }),
+    });
 
-    this.employeeForm.get('contactPreference').valueChanges.subscribe((data:string) => {
-      debugger
-      this.validatePhone(data)
-    })
+    this.employeeForm
+      .get("contactPreference")
+      .valueChanges.subscribe((data: string) => {
+        debugger;
+        this.validatePhone(data);
+      });
 
-    this.employeeForm.valueChanges.subscribe(
-      data => this.logValidationErrors(this.employeeForm)
-      )
-  } 
-  onSubmit() : void {
+    this.employeeForm.valueChanges.subscribe((data) =>
+      this.logValidationErrors(this.employeeForm)
+    );
+  }
+  onSubmit(): void {
     console.log(this.employeeForm);
-    
   }
-  validatePhone(selectedValue:string) {
-    debugger
-    const phone = this.employeeForm.get('phone')
-    const email = this.employeeForm.get('email')
-    if(selectedValue == 'phone') {
-      email.clearValidators()
-      phone.setValidators(Validators.required)
-      phone.updateValueAndValidity()
+  validatePhone(selectedValue: string) {
+    debugger;
+    const phone = this.employeeForm.get("phone");
+    const email = this.employeeForm.get("email");
+    if (selectedValue === "phone") {
+      email.clearValidators();
+      phone.setValidators(Validators.required);
+      phone.updateValueAndValidity();
+    } else {
+      phone.clearValidators();
+      email.setValidators(Validators.required);
     }
-    else {
-      phone.clearValidators()
-      email.setValidators(Validators.required)
-    }
-    phone.updateValueAndValidity()
+    phone.updateValueAndValidity();
   }
-  logValidationErrors(group: FormGroup = this.employeeForm) : void {
-    Object.keys(group.controls).forEach((key:string) => 
-    {
-      const abstractControl  =  group.get(key)
-      if(abstractControl instanceof FormGroup)
-      {
-        this.logValidationErrors(abstractControl)
-      }
-      else {
-          this.formErrors[key] = ''
-          if(abstractControl && !abstractControl.valid && (abstractControl.dirty || abstractControl.touched))
-          {
-            const messages = this.validationMessages[key]
-            const errors = abstractControl.errors
-            if(errors)
-            {
-              for(const errorKey in errors)
-              {
-                this.formErrors[key] += messages[errorKey] + ' '
-              }
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      } else {
+        this.formErrors[key] = "";
+        if (
+          abstractControl &&
+          !abstractControl.valid &&
+          (abstractControl.dirty || abstractControl.touched)
+        ) {
+          const messages = this.validationMessages[key];
+          const errors = abstractControl.errors;
+          if (errors) {
+            for (const errorKey in errors) {
+              this.formErrors[key] += messages[errorKey] + " ";
             }
           }
+        }
       }
-    })
+    });
   }
-  onLoadData() : void {
-    
-  }
+  onLoadData(): void {}
+}
+
+function emailDomain(domain: string) {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const email = control.value;
+    const emailDomain = email.substring(email.lastIndexOf("@") + 1);
+    if (email === "" || emailDomain.toLowerCase() === domain.toLowerCase())
+      return null;
+    else return { emailDomain: true };
+  };
 }
